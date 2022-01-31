c9b74a8da61a5824c47dd206a09f780ec68b341d
Paul Cutmore <paul.cutmore@icloud.com>
2022-01-31 08:36:44 +0000

    BUGFIX: Replaced 'find ./' with 'find .' to fix for MacOS

--- init.fish
+++ init.fish
@@ -7,7 +7,7 @@
 
 function git-refresh --on-variable PWD \
   --description "git pull automatically wherever inside a git repository"
-    set --local hasGit (find ./ -maxdepth 1 -type d -name .git -print)
+    set --local hasGit (find . -maxdepth 1 -type d -name .git -print)
     if test "$hasGit" = "./.git"
         echo -e "\e[1m(git-refresh) - GIT repo detected\e[0m"
         git pull --all --verbose
